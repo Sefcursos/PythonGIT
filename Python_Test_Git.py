8b042ee75f7f51572d44d9ae2fc9c3ae19346194
Jennifer-Jimenez-25 <jimenezmorenojennifer1@gmail.com>
2025-04-25 19:59:31 +0200

    Commit he agregado un comentario

--- Python_Test_Git.py
+++ Python_Test_Git.py
@@ -22,3 +22,5 @@ print("!Holaaa___Holaaaa")
 # Quiero agregar este comentario, para probar si menciona el push antes que el commit o el commit antes que el push
 
 # No passa nada, estoy probando otro comentario con respecto a las subidas de los archivos
+
+# Hola soy Jennifer
